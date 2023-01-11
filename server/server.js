a807fa63e30a2ef70b1851816a7fe9f116028929
Elena Mederos <elenamc91@hotmail.com>
2023-01-11 17:25:52 +0100

    Configuración de variables de entorno

---
+++ server/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require("express");
+const router = require("./routes/routes");
+const port = process.env.PORT;
+const app = express();
+const cookieParser = require('cookie-parser');
+
+// Middlewares: 
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.listen(port, () => console.log(`Server ON: ${port}`));
