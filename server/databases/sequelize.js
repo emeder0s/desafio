24f2510a0bfa533b573e6bc52cba44f645f84630
Elena Mederos <elenamc91@hotmail.com>
2023-01-12 11:00:28 +0100

    Variables de entorno y actualización del script de la db

--- server/databases/sequelize.js
+++ server/databases/sequelize.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 const connection = {
     open: async () => {
         const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-            host: process.env.HOS,
+            host: process.env.HOST,
             timezone: '+01:00',
             dialect: 'mysql',
             port: DB_PORT
